410060defe291a880af8f5cee8bb187638700fb1
Kim Kyu San <developerstarlight@gmail.com>
2020-04-20 17:28:58 +0900

    clush into cluster

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const fnfExc = require('./error/io/FileNotFoundException')
 const Error = require('./error/Error')
 const checker = require('./lib/qnpfrChecker')
 
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
+
 const target = process.argv[2]
 
 checker.check(target)
